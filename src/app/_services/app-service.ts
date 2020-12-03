f05935132ca468902262c85da6275d61a3976e92
timyshark <tim.a.hass@outlook.com>
2020-12-02 18:57:55 -0800

    3 files modified

--- src/app/_services/app-service.ts
+++ src/app/_services/app-service.ts
@@ -1,31 +1,27 @@
-import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable, of, Subject, Subscriber } from 'rxjs';
+import { Injectable } from "@angular/core";
+import { BehaviorSubject, Observable, of, Subject, Subscriber } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class AppService {
-
-restAPIcall(){
-  function appAPIEmitter(appSubscriber : Subscriber<String>) {
-     appSubscriber.next('Hello');
-     appSubscriber.next('Welcome');
-     appSubscriber.next("That's All.");
-     appSubscriber.complete();
-  }
-  return new Observable<String>(appAPIEmitter);
-}
-vuCall(){
-  function vuEmitter(vuSubscriber : Subscriber<String>) {
-     this.restAPIcall()
-     .subscribe(
-       (s)=> {
-         console.log('Recieved :' + s);
-         vuSubscriber.next('Vu - ' + s);
-         vuSubscriber.complete();
-       }
-     )
+  restAPIcall(): Observable<string> {
+    function appAPIEmitter(appSubscriber: Subscriber<String>) {
+      appSubscriber.next("Hello");
+      appSubscriber.next("Welcome");
+      appSubscriber.next("That's All.");
+      appSubscriber.complete();
     }
-  return new Observable<String>(vuEmitter);
-}
+    return new Observable<string>(appAPIEmitter);
+  }
+  vuCall(): Observable<string> {
+    function vuEmitter(vuSubscriber: Subscriber<string>) {
+      this.restAPIcall().subscribe(s => {
+        console.log("Recieved :" + s);
+        vuSubscriber.next("Vu - " + s);
+        vuSubscriber.complete();
+      });
+    }
+    return new Observable<string>(vuEmitter);
+  }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,28 @@
-import { Component, VERSION } from '@angular/core';
-import { AppService } from './_services/app-service';
+import { Component, VERSION } from "@angular/core";
+import { AppService } from "./_services/app-service";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
-  constructor(private appService:AppService){
-    
+export class AppComponent {
+  name = "Angular " + VERSION.major;
+  appMessages: string[] = new Array();
+  vuMessages: string[] = new Array();
+  constructor(private appService: AppService) {}
+  getAppData() {
+    console.log("Start getting app data");
+    this.appService.restAPIcall().subscribe((s: string) => {
+      this.appMessages.push(s);
+      console.log("Recieved :" + s);
+    });
   }
-  getAppData(){
-    console.log('Start getting app data');
-    this.appService.restAPIcall()
-     .subscribe(
-       (s)=> {
-         console.log('Recieved :' + s);
-       }
-     )
-  }
-  getVuData(){
-      console.log('Start getting view data');
-    this.appService.vuCall()
-     .subscribe(
-       (s)=> {
-         console.log('Recieved :' + s);
-       }
-     )
+  getVuData() {
+    console.log("Start getting view data");
+    this.appService.vuCall().subscribe((s: string) => {
+      this.vuMessages.push(s);
+      console.log("Recieved :" + s);
+    });
   }
 }
